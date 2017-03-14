2591a1db4671e897abf6c16633f796b1295368c3
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-14 20:41:01 +0100

    added html page to show colors

---
+++ colors.js
@@ -0,0 +1,31 @@
+const backgroundColor = 'rgba(41, 44, 54, 0.9)';
+const foregroundColor = '#ffffff';
+const borderColor = 'transparent';
+const cursorColor = 'magenta';
+
+const colors = {
+  black: '#292c36',
+  red: '#dc3c3c',
+  green: '#65888d',
+  yellow: '#d28e5d',
+  blue: '#4670d8',
+  magenta: '#b73999',
+  cyan: '#00aba5',
+  white: '#999999',
+  lightBlack: '#333333',
+  lightRed: '#666666',
+  lightGreen: '#52bd58',
+  lightYellow: '#05c76f',
+  lightBlue: '#4670d8',
+  lightMagenta: '#8485ce',
+  lightCyan: '#00aba5',
+  lightWhite: '#ffffff',
+};
+
+if (typeof module === undefined) module.exports= {
+  backgroundColor,
+  foregroundColor,
+  borderColor,
+  cursorColor,
+  colors,
+};
--- index.js
+++ index.js
@@ -1,28 +1,12 @@
+const {
+  backgroundColor,
+  foregroundColor,
+  borderColor,
+  cursorColor,
+  colors,
+} = require('./colors');
+
 module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
-
-const backgroundColor = 'rgba(41, 44, 54, .7)';
-const foregroundColor = '#ffffff';
-const borderColor = 'transparent';
-const cursorColor = 'magenta';
-
-const colors = {
-  black: '#292c36',
-  red: '#dc3c3c',
-  green: '64878f',
-  yellow: '#d28e5d',
-  blue: '#4670d8',
-  magenta: '#b73999',
-  cyan: '#00aba5',
-  white: '#999999',
-  lightBlack: '#333333',
-  lightRed: '#666666',
-  lightGreen: '#52bd58',
-  lightYellow: '#05c76f',
-  lightBlue: '#4670d8',
-  lightMagenta: '#8485ce',
-  lightCyan: '#00aba5',
-  lightWhite: '#ffffff',
-};
 
 exports.decorateConfig = config => Object.assign({}, config, {
   fontSize: 18,
@@ -38,6 +22,7 @@ exports.decorateConfig = config => Object.assign({}, config, {
 
     x-row {
       font-variant-ligatures: initial;
+      line-height: 23px;
     }
 
     .cursor-node[focus=true] {
