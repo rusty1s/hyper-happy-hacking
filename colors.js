597538f2768adb80d01e62cc9ad61a9a762eb09b
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-24 18:25:26 +0100

    removed unused colors

--- colors.js
+++ colors.js
@@ -1,22 +1,3 @@
-const wwdc16 = {
-  black: '#292c36',
-  red: '#dc3c3c',
-  green: '#64878f',
-  yellow: '#d28e5d',
-  blue: '#4670d8',
-  magenta: '#b73999',
-  cyan: '#00aba5',
-  white: '#999999',
-  lightBlack: '#333333',
-  lightRed: '#666666',
-  lightGreen: '#52bd58',
-  lightYellow: '#95c76f',
-  lightBlue: '#4670d8',
-  lightMagenta: '#8485ce',
-  lightCyan: '#00aba5',
-  lightWhite: '#ffffff',
-};
-
 const happyHacking = {
   black: '#212121',
   lightBlack: '#323232',
@@ -32,27 +13,8 @@ const happyHacking = {
   lightMagenta: '#ce839d',
   cyan: '#00aba5',
   lightCyan: '#00aba5',
-  white: '#999999',
+  white: '#666666',
   lightWhite: '#eee8d5',
-};
-
-const solarizedDark = {
-  black: '#002b36',
-  lightBlack: '#073642',
-  red: '#dc322f',
-  lightRed: '#cb4b16',
-  green: '#859900',
-  lightGreen: '#586e75',
-  yellow: '#b58900',
-  lightYellow: '#657b83',
-  blue: '#268bd2',
-  lightBlue: '#839496',
-  magenta: '#d33682',
-  lightMagenta: '#6c71c4',
-  cyan: '#2aa198',
-  lightCyan: '#93a1a1',
-  white: '#eee8d5',
-  lightWhite: '#fdf7e3',
 };
 
 const colors = happyHacking;
