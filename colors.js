92ff55b713f2afdcc112957471b4ab67aa3a08e4
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-24 20:08:31 +0100

    added license

--- colors.js
+++ colors.js
@@ -1,23 +1,22 @@
-const happyHacking = {
+const colors = {
   black: '#212121',
-  lightBlack: '#323232',
   red: '#dc3c3c',
-  lightRed: '#dc3c3c',
-  green: '#55a087',
-  lightGreen: '#55a087',
-  yellow: '#b58900',
-  lightYellow: '#b58900',
-  blue: '#65d7fd',
-  lightBlue: '#65d7fd',
+  green: '#689d6a',
+  yellow: '#d79921',
+  blue: '#458588',
   magenta: '#ce839d',
-  lightMagenta: '#ce839d',
   cyan: '#00aba5',
+  white: '#a89984',
+  lightBlack: '#3c3836',
+  lightRed: '#dc3c3c',
+  lightGreen: '#689d6a',
+  lightYellow: '#d79921',
+  lightBlue: '#458588',
+  lightMagenta: '#ce839d',
   lightCyan: '#00aba5',
-  white: '#666666',
   lightWhite: '#eee8d5',
 };
 
-const colors = happyHacking;
 const backgroundColor = colors.black;
 const foregroundColor = colors.lightWhite;
 const borderColor = 'transparent';
