f43bba3be0a96ce4b49da95e24b4d6164fb8e532
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-24 19:33:21 +0100

    added new weights for font

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
 
 exports.decorateConfig = config => Object.assign({}, config, colors, {
   fontSize: 18,
-  fontFamily: '"Fira Code"',
+  fontFamily: '"Fira Code", monospace',
   cursorShape: 'BLOCK',
   padding: '0 10px',
   termCSS: `
     ${config.termCSS || ''}
+
+    @font-face {
+      font-family: "Fira Code";
+      font-weight: normal;
+      font-style: normal;
+      src: url(file:///${__dirname}/FiraCode-Light.otf);
+    }
+
+    @font-face {
+      font-family: "Fira Code";
+      font-weight: bold;
+      font-style: normal;
+      src: url(file:///${__dirname}/FiraCode-Medium.otf);
+    }
 
     x-row {
       font-variant-ligatures: initial;
