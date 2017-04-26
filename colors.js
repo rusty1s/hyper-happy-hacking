a88aca4be9facd4677083cbff0b770948741ffda
rusty1s <matthias.fey@tu-dortmund.de>
2017-04-26 17:32:00 +1000

    added rgb colors

--- colors.js
+++ colors.js
@@ -1,23 +1,23 @@
 const colors = {
-  black: '#282828',
-  red: '#dc3c3c',
-  green: '#689d6a',
-  yellow: '#d79921',
-  blue: '#458588',
-  magenta: '#ce839d',
-  cyan: '#00aba5',
-  white: '#a89984',
-  lightBlack: '#3c3836',
-  lightRed: '#dc3c3c',
-  lightGreen: '#689d6a',
-  lightYellow: '#d79921',
-  lightBlue: '#458588',
-  lightMagenta: '#ce839d',
-  lightCyan: '#00aba5',
-  lightWhite: '#eee8d5',
+  black: '#282828',  // 40,40,40
+  red: '#dc3c3c',  // 220,60,60
+  green: '#689d6a',  // 104,157,106
+  yellow: '#d79921',  // 215,153,33
+  blue: '#458588',  // 69,133,136
+  magenta: '#ce839d',  // 206,131,157
+  cyan: '#00aba5',  // 0,171,165
+  white: '#a89984',  // 168,153,132
+  lightBlack: '#3c3836',  // 60,56,54
+  lightRed: '#dc3c3c',  // 220,60,60
+  lightGreen: '#689d6a',  // 104,157,106
+  lightYellow: '#d79921',  // 215,153,33
+  lightBlue: '#458588',  // 69,133,136
+  lightMagenta: '#ce839d',  // 206,131,157
+  lightCyan: '#00aba5',  // 0,171,165
+  lightWhite: '#eee8d5',  // 238,232,213
 };
 
-const backgroundColor = '#212121';
+const backgroundColor = '#212121';  // 33,33,33
 const foregroundColor = colors.lightWhite;
 const borderColor = 'transparent';
 const cursorColor = colors.magenta;
